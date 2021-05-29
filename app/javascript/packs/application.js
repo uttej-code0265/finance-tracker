1bd598baed6da579bb52bdd2443237aa8a233a09
Uttej Kumar <uttejkumarponugoti@gmail.com>
2021-05-29 10:55:13 +0530

    Adding JAVA SCRIPT and BOOT STRAP Views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,6 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import "bootstrap"
