c5ad2013d762fcc0bc46d00564486142203c44da
Uttej Kumar <uttejkumarponugoti@gmail.com>
2021-06-02 10:00:55 +0530

    temp

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,9 +3,10 @@ class StocksController < ApplicationController
         if params[:stock].present?
             @stock=Stock.new_lookup(params[:stock])
             if @stock
-                respond_to do |format|
-                    format.js {render partial: 'users/result'}
-                end
+                # respond_to do |format|
+                #     format.js {render partial: 'users/result'}
+                # end
+                render 'users/my_portfolio'
             else
                 respond_to do |format|
                     flash.now["alert"]="Please enter valid symbol"
